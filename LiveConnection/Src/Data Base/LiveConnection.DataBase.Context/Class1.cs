867283ae6cf7922dcc053c68903e1f60b0630c59
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-21 01:23:47 +0330

    delete default files from all projects

--- LiveConnection/Src/Data Base/LiveConnection.DataBase.Context/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace LiveConnection.DataBase.Context
-{
-    public class Class1
-    {
-
-    }
-}
--- LiveConnection/Src/Data Base/LiveConnection.DataBase.Entity/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace LiveConnection.DataBase.Entity
-{
-    public class Class1
-    {
-
-    }
-}
