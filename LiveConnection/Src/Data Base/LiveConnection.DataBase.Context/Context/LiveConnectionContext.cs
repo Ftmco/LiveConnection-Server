bbf10be0a7f8b34632e2ec7c0f27d6eff0d9592a
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-21 12:35:40 +0330

    Create LiveConnectionContext.cs

---
+++ LiveConnection/Src/Data Base/LiveConnection.DataBase.Context/Context/LiveConnectionContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LiveConnection.DataBase.Context;
+
+public class LiveConnectionContext : DbContext
+{
+    public static string ConnectionString { get; set; } = null!;
+
+    public LiveConnectionContext(DbContextOptions<LiveConnectionContext> options) : base(options)
+    { }
+
+    public LiveConnectionContext()
+    { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+            optionsBuilder.UseSqlServer(ConnectionString);
+    }
+}
